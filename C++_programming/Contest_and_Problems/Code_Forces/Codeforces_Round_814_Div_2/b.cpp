dd024053238732b77f2bcb5040ed3845cf48c9ef
Pritom Paul <pritompaul1920@gmail.com>
2022-08-16 23:04:04 +0600

    New Cp

---
+++ C++_programming/Contest_and_Problems/Code_Forces/Codeforces_Round_814_Div_2/b.cpp
@@ -0,0 +1,147 @@
+//#include <iostream>
+#include <bits/stdc++.h>
+
+// Data Types
+#define ll long long
+#define lli long long int
+#define ld long double
+typedef unsigned long int uint32;
+typedef unsigned long long int uint64;
+// Constant Values
+#define pi acos(-1)
+const ll Mod = 1e9 + 7;
+// File handling
+#define f_input freopen("input.txt", "r", stdin)
+#define f_output freopen("output.txt", "w", stdout)
+#define nl '\n'
+#define FastIO ios_base::sync_with_stdio(false), cin.tie(NULL)
+// Loops
+#define fs(i, s, e) for (int i = s; i < e; i++)
+#define fo(i, e) for (int i = 0; i < e; i++)
+#define fr(i, s, e) for (int i = s; i >= e; i--)
+#define fi(n) for (int i = 0; i < n; i++)
+// Strings
+#define all(x) x.begin(), x.end()
+#define rev(v) reverse(v.begin(), v.end())
+#define srt(v) sort(v.begin(), v.end())
+// Array
+#define mems(a, x) memset(a, x, sizeof(a))        // Works only for 0 and -1
+#define ass_vel(ar, n, x) fo(i, n) ar[i] = x      // Set value in array
+#define parr(ar, n) fo(i, n) cout << ar[i] << " " // Print array
+#define eol cout << endl
+#define a_sort(ar, size) sort(ar, ar + size)
+#define d_sort(arr, size) sort(arr, arr + size, greater<int>())
+#define min_el(arr, size) *min_element(arr, arr + size)
+#define max_el(arr, size) *max_element(arr, arr + size)
+#define ain(arr, n) fo(i, n) cin >> arr[i]
+#define find_(a, n, x) find(a, a + n, x) - a
+// cout << fixed << setprecision(__n) << x << endl;
+#define FSP(x) fixed << setprecision(x)
+#define pop_cnt(n) __builtin_popcount(n) // Assign it to a value to find the number of 1 in a binary number
+#define numBits(n) log2(n) + 1           // Assign it to a value to get the number of bits in an integer
+// Vector
+#define vi vector<int>
+#define vll vector<ll>
+#define vc vector<char>
+#define vs vector<string>
+#define pb push_back
+// debug
+#define cpoint cout << "_________________CHECK POINT_________________\n";
+#define _debug(x) cout << x << endl
+#define Yes cout << "YES\n";
+#define No cout << "NO\n";
+
+using namespace std;
+
+void solve()
+{
+    int n, k;
+    cin >> n >> k;
+
+    if (k == 0)
+    {
+        cout << "NO" << nl;
+    }
+    else
+    {
+        int ck = 0;
+        for (int i = 1; i <= n; i += 2)
+        {
+            int a = i, b = i + 1;
+            if ((((a + k) * b) % 4 != 0) || ((b + k) * a) % 4 != 0)
+            {
+                ck = 1;
+                break;
+            }
+        }
+
+        if (ck == 1)
+        {
+            cout << "NO" << nl;
+            return;
+        }
+        else
+        {
+            cout << "YES" << nl;
+        }
+
+        for (int i = 1; i <= n; i += 2)
+        {
+            int a = i, b = i + 1;
+            if (((a + k) * b) % 4 == 0)
+            {
+                cout << a << " " << b << nl;
+            }
+            else if (((b + k) * a) % 4 == 0)
+            {
+                cout << b << " " << a << nl;
+            }
+
+            // else
+            // {
+            //     cout <<
+            // }
+        }
+    }
+}
+
+int main()
+{
+    FastIO;
+#ifndef ONLINE_JUDGE
+    double start = clock();
+    f_input;
+    f_output;
+#endif
+
+    int ttt;
+    cin >> ttt;
+    while (ttt--)
+        solve();
+
+#ifndef ONLINE_JUDGE
+    double time = (clock() - start) / CLOCKS_PER_SEC;
+    cerr << "Running Time : " << time << "\n";
+#endif
+    return 0;
+}
+
+/***************************************************************************************************\
+*                                            Written by:                                            *
+*                                                                                                   *
+*     ██▓███   ██▀███   ██▓▄▄▄█████▓ ▒█████   ███▄ ▄███▓     ██▓███   ▄▄▄       █    ██  ██▓        *
+*    ▓██░  ██▒▓██ ▒ ██▒▓██▒▓  ██▒ ▓▒▒██▒  ██▒▓██▒▀█▀ ██▒    ▓██░  ██▒▒████▄     ██  ▓██▒▓██▒        *
+*    ▓██░ ██▓▒▓██ ░▄█ ▒▒██▒▒ ▓██░ ▒░▒██░  ██▒▓██    ▓██░    ▓██░ ██▓▒▒██  ▀█▄  ▓██  ▒██░▒██░        *
+*    ▒██▄█▓▒ ▒▒██▀▀█▄  ░██░░ ▓██▓ ░ ▒██   ██░▒██    ▒██     ▒██▄█▓▒ ▒░██▄▄▄▄██ ▓▓█  ░██░▒██░        *
+*    ▒██▒ ░  ░░██▓ ▒██▒░██░  ▒██▒ ░ ░ ████▓▒░▒██▒   ░██▒    ▒██▒ ░  ░ ▓█   ▓██▒▒▒█████▓ ░██████▒    *
+*    ▒▓▒░ ░  ░░ ▒▓ ░▒▓░░▓    ▒ ░░   ░ ▒░▒░▒░ ░ ▒░   ░  ░    ▒▓▒░ ░  ░ ▒▒   ▓▒█░░▒▓▒ ▒ ▒ ░ ▒░▓  ░    *
+*    ░▒ ░       ░▒ ░ ▒░ ▒ ░    ░      ░ ▒ ▒░ ░  ░      ░    ░▒ ░       ▒   ▒▒ ░░░▒░ ░ ░ ░ ░ ▒  ░    *
+*    ░░         ░░   ░  ▒ ░  ░      ░ ░ ░ ▒  ░      ░       ░░         ░   ▒    ░░░ ░ ░   ░ ░       *
+*                ░      ░               ░ ░         ░                      ░  ░   ░         ░  ░    *
+*                                           ╔╗╔╔═╗╦ ╦╔╗                                             *
+*                                        ───║║║║╣ ║ ║╠╩╗───                                         *
+*                                           ╝╚╝╚═╝╚═╝╚═╝                                            *
+*                                 ╔╦╗┌─┐┌─┐┌┬┐   ┌─┐┌─┐  ╔═╗╔═╗╔═╗                                  *
+*                              ─── ║║├┤ ├─┘ │    │ │├┤   ║  ╚═╗║╣ ───                               *
+*                                 ═╩╝└─┘┴   ┴ o  └─┘└    ╚═╝╚═╝╚═╝                                  *
+\***************************************************************************************************/
--- C++_programming/Contest_and_Problems/Random_Practice/CodeForces/testCode.cpp
+++ C++_programming/Contest_and_Problems/Random_Practice/CodeForces/testCode.cpp
@@ -2,9 +2,45 @@
 using namespace std;
 int main()
 {
-    string s;
-    getline(cin, s);
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n, k, i;
+        cin >> n >> k;
+        if (k == 0)
+            cout << "NO" << endl;
+        else
+        {
+            cout << "YES" << endl;
+            if (k % 2 != 0)
+            {
+                for (i = 1; i <= n; i++)
+                {
+                    cout << i << " ";
+                    if (i % 2 == 0)
+                    {
+                        cout << endl;
+                    }
+                }
+            }
+            else
+            {
 
-    cout << s;
-
+                for (i = n; i >= 1; i--)
+                {
+                    if ((i + 1) % 4 == 0)
+                        cout << i + 1 << " ";
+                    else if ((i) % 4 == 0)
+                        cout << i - 1 << " ";
+                    else
+                        cout << i << " ";
+                    if (i % 2 == 1)
+                    {
+                        cout << endl;
+                    }
+                }
+            }
+        }
+    }
 }
